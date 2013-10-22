a7851a8a355cb586a0358e642d966a3768975e37
Hackbright Student <user@chromebox-002.(none)>
2013-10-21 18:03:24 -0700

    Facebook excercise, in dev

--- app.py
+++ app.py
@@ -6,15 +6,58 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        # model.connect_to_db()
+        #username = model.get_user_by_id(session.get("user_id"))
+        # return redirect(url_for('view_user', username=username))
+        return "Hello, user %d"%session.get("username")
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    model.connect_to_db()
+    username = request.form.get("username")
+    password = request.form.get("password")
+    user_id = model.authenticate(username, password)
+    if user_id != None:
+        flash("User authenticated!")
+        session["user_id"] = user_id
+        return redirect(url_for('view_user', username=username))
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+        return redirect(url_for("index"))
 
-@app.route("/youshouldprobablychangethisurl")
+
+@app.route("/user/<username>")
+def view_user(username):
+    model.connect_to_db()
+    user_id = model.get_user_by_name(username)
+    posts = model.get_wall_posts(user_id[0])
+    return render_template("wall.html", posts = posts,
+                                        logged_in = session["user_id"],
+                                        username = username)
+
+@app.route("/post", methods=["POST"])
+def post_to_wall():
+    model.connect_to_db()
+    user_id = session['user_id']
+    author_name = model.get_user_by_id(user_id[0])
+    content = request.form.get("content")
+    print content
+    username = request.form.get("username")
+    print username
+    model.create_wallpost(author_name, content, username)
+    return redirect(url_for("view_user", username = username))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,71 @@
-ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+import sqlite3
+import datetime
+
+# ADMIN_USER="hackbright"
+# ADMIN_PASSWORD=5980025637247534551
+
+DB = None
+CONN = None
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
 
 def authenticate(username, password):
-    return False
+    query = """SELECT id FROM users WHERE username = ? AND password = ?"""
+    DB.execute(query, (username, password,))
+    user_id = DB.fetchone()
+    return user_id
+
+def get_user_by_name(username):
+    query = """SELECT id FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    user_id = DB.fetchone()
+    return user_id
+
+def get_user_by_id(user_id):
+    query = """SELECT username FROM users WHERE id = ?"""
+    DB.execute(query, (user_id,))
+    username = DB.fetchone()
+    return username[0]
+
+def get_wall_posts(user_id):
+    query = """SELECT content, author_id, created_at FROM wall_posts WHERE owner_id = ?"""
+    DB.execute(query,(user_id,))
+    posts = DB.fetchall()
+    posts_result = []
+    for post in posts:
+        count = 0
+        line = []
+        for token in post:
+            if count == 1:
+                author_id = token
+                query = """SELECT username FROM users WHERE id =?"""
+                DB.execute(query, (author_id,))
+                author = DB.fetchone()
+                token = author[0]
+            line.append(token)
+            count += 1
+        posts_result.append(line)
+    return posts_result
+
+def create_wallpost(author_id, content, owner_id):
+    post_date = datetime.datetime.now()
+    author = get_user_by_name(author_id)
+    owner = get_user_by_name(owner_id)
+    query = """INSERT INTO wall_posts VALUES (null, ?, ?, ?, ?)"""
+    DB.execute(query, (owner, author, post_date, content,))
+    CONN.commit()
+    return "posted!"
+
+
+# def insert():
+#     query = """insert into wall_posts values (null, 0, 0, ?, ?)"""
+#     DB.execute(query, (datetime.datetime.now(), "Some text") )
+	
+def main():
+    connect_to_db()
+
+if __name__ == "__main__":
+   main()
